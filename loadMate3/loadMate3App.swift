39e22e9c3c86e97c563b5560db71d8a1670b8ac5
scottmatheson58 <scott@matheson.it>
2026-03-26 17:33:36 +0000

    Initial Commit

---
+++ loadMate3/loadMate3App.swift
@@ -0,0 +1,17 @@
+//
+//  loadMate3App.swift
+//  loadMate3
+//
+//  Created by scott matheson on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct loadMate3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
